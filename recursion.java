b9d34d0d5c87ff1638c9390163054f18351508f5
Amruta-0 <164344736+Amruta-0@users.noreply.github.com>
2024-09-25 19:10:50 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,17 @@
+class recursion
+{
+static int count=0;
+static void p()
+{
+count++;
+if(count <=5)
+{
+System.out.println("hello"+count);
+p();
+}
+}
+public static void main(String args[])
+{
+p();
+}
+}
